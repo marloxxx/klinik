9363e1cf6ead171b43179de275af8217340977c0
marloxxx <66661134+marloxxx@users.noreply.github.com>
2022-11-05 19:48:55 +0700

    Initial commit

---
+++ app/Models/MedicalRecordMedicine.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MedicalRecordMedicine extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'medical_record_id',
+        'medicine_id',
+        'quantity',
+    ];
+
+    public function medical_record()
+    {
+        return $this->belongsTo(MedicalRecord::class);
+    }
+
+    public function medicine()
+    {
+        return $this->belongsTo(Medicine::class);
+    }
+}
